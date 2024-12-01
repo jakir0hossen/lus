b059e3c39364d3180fc594f59c194797dd72da3c
jakir0hossen <jhbjakir@gmail.com>
2024-12-01 20:20:20 +0600

    lucc

--- lucc.php
+++ lucc.php
@@ -78,7 +78,7 @@
         <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
         <div class="user-info">
           <h5>July Dec</h5>
-          <small>2h ago</small>
+          <small>3h ago</small>
         </div>
       </div>
     </div>
