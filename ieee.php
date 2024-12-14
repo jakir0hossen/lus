234343bf45497c999f8ba24edfacc6399bb7c243
jakir0hossen <jhbjakir@gmail.com>
2024-12-14 20:12:06 +0600

    ieee

---
+++ ieee.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>LU Magazine</title>
+    
+    <script src="https://kit.fontawesome.com/eaa4609b2f.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/79d1bfb033.js" crossorigin="anonymous"></script>
+   
+    <link rel="stylesheet" href="./ieee-style.css" />
+</head>
+
+<body>
+<nav>
+        <div class="container">
+            <div class="logo">
+                <img src="../images/logo.png" alt="">
+            </div>
+            
+
+            <div class="navbar">
+                <ul>
+                    <li><a href="../Home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="#">LU</a>
+                     <ul>
+                        <li><a href="./blog.php">Blogs</a></li>
+                        <li><a href="./books.php">Book</a></li>
+                        <li><a href="./event.php">Event</a></li>
+                        <li><a href="#">Lu Club</a>
+                            <ul>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUSSC</a></li>
+                                <li><a href="#">LUEC</a></li>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUDC</a></li>
+                                <li><a href="#">LUMUNA</a></li>
+                                <li><a href="#">LU BNCC</a></li>
+                                <li><a href="#">LUPS</a></li>
+                                <li><a href="#">LUPS</a></li>
+                            </ul>
+                        </li>
+                     </ul>
+                    </li>
+                    <li><a href="./news.php">News</a></li>
+                    <li><a href="#">Gallery</a>
+                        
+                        <ul>
+                            <li><a href="./photo.php">Photo & Video</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="./contact.php">Contact</a></li>
+                    <li><a href="./logout.php"><i class="fa-solid fa-right-to-bracket"></i>logout</a></li>
+                </ul>
+                
+            </div>
+
+        </div>
+</nav>
+
+<section class="container-section">
+<div class="container-card">
+        <h1>IEEE Computer Society LU SB Chapter
+
+</h1>
+       <div class="main-card">
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Ieee/JAC102.webp" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Md. Jehadul Islam Mony
+
+                 </div>
+                 <div class="job">Lecturer <br>
+                 Computer Science & Engineering <br>
+                 Advisor <br>
+                 IEEE Computer Society LU SB Chapter
+                 </div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Ieee/JAC101.webp" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Md. Jalal Uddin Chowdhury
+
+                 </div>
+                 <div class="job">Lecturer
+                 <br>
+                 Computer Science & Engineering <br>
+                 Co-Advisor <br>
+                 IEEE Computer Society LU SB <br>
+</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Ieee/JAC103.webp" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Md. Mahdi Hossain Hira
+                 </div>
+                 <div class="job">Adjunct Lecturer
+                 <br>
+                 Computer Science & Engineering <br>
+                 Co-Advisor <br>
+                 Leading University Computer Club (LUCC)</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+        
+       </div>
+      
+    </div>
+</section>
+
+<section class="gallery-section" >
+<div class="gallery-div">
+
+<div class="gallery-wrap">
+    <div class="gallery-images">
+        <div class="gallery">
+          <div class="image-section">
+            <img src="./Ieee/ieee.jpg" alt="">
+          </div>
+          <div class="caption">
+            <h3> 𝐓𝐢𝐦𝐞 𝐢𝐬 𝐫𝐮𝐧𝐧𝐢𝐧𝐠 𝐨𝐮𝐭! 🚨
+            𝐑𝐞𝐠𝐢𝐬𝐭𝐞𝐫 𝐍𝐨𝐰!</h3>
+            <p>Join us on the 𝐈𝐄𝐄𝐄 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐋𝐔 𝐒𝐁 𝐂𝐡𝐚𝐩𝐭𝐞𝐫 Presents, 𝐌𝐞𝐦𝐛𝐞𝐫𝐬𝐡𝐢𝐩 𝐂𝐚𝐦𝐩𝐚𝐢𝐠𝐧 event "𝐇𝐨𝐠𝐰𝐚𝐫𝐭𝐬 𝐨𝐟 𝐈𝐧𝐧𝐨𝐯𝐚𝐭𝐢𝐨𝐧!" Experience a magical journey of innovation, technology, and growth.
+            </p>
+          </div>
+        </div>
+        <div class="gallery">
+            <div class="image-section">
+              <img src="./Ieee/ieee.j-1pg.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>IEEE Computer Society LU SB</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Ieee/ieee.j-2pg.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>IEEE Computer Society LU SB</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Ieee/ieee.j-3pg.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>🌟 𝐃𝐨𝐛𝐛𝐲 𝐢𝐬 𝐋𝐨𝐬𝐭! 𝐁𝐮𝐭 𝐰𝐡𝐞𝐫𝐞 𝐰𝐢𝐥𝐥 𝐭𝐡𝐞 𝐩𝐨𝐫𝐭𝐚𝐥 𝐭𝐚𝐤𝐞 𝐡𝐢𝐦? 🌟</h3>
+              <p>Will it be a realm brimming with magic or one where the term doesn’t even exist?
+              🪄 If you’re under the spell of curiosity, then prepare yourself for an enchanting journey
+              </p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Ieee/ieee.j-4pg.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>IEEE Computer Society LU SB</h3>
+              <p>Acting Dean of the Faculty of Modern Science, and Divisional Coordinator of IEEE CS BDC, for his outstanding role as the Chief Invigilator in the IEEE CS LU SBC Election 2024.</p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Ieee/ieee.j-5pg.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>IEEE Computer Society LU SB</h3>
+              <p></p>
+            </div>
+          </div>
+    </div>
+</div>
+
+</div>
+</section>
+   
+  
+    <footer>
+        <div class="footerContainer">
+            <div class="socialIcons">
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-google-plus"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                
+            </div>
+            <div class="footerNav">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Lu</a></li>
+                </ul>
+            </div>
+            
+        </div>
+
+        <div class="footerBottom">
+            <p>Copyright &copy;2024; Design by <span class="designer">LuMagazine</span></p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+
+
+
+
+
