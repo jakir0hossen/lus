dbeef60993a95a539d6fd36bbab371213f695ce4
jakir0hossen <jhbjakir@gmail.com>
2024-12-14 19:48:41 +0600

    lucc complete

--- lucc.php
+++ lucc.php
@@ -58,8 +58,183 @@
             </div>
 
         </div>
-    </nav>
+</nav>
 
+<section class="container-section">
+<div class="container-card">
+        <h1>Leading University Computer Club (LUCC)
+
+</h1>
+       <div class="main-card">
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Lucc/jahid-1-scaled-1.jpg" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Kazi Md. Jahid Hasan
+                 </div>
+                 <div class="job">Assistant Professor <br>
+                 Computer Science & Engineering <br>
+                 Advisor <br>
+                 Leading University Computer Club (LUCC)</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Lucc/ebrahim1-scaled-1.jpg" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Md. Ebrahim Hossain
+                 </div>
+                 <div class="job">Assistant Professor <br>
+                 Computer Science & Engineering <br>
+                 Co-advisor <br>
+Leading University Computer Club (LUCC) <br>
+</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Lucc/pitiraj1-scaled-1.jpg" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Prithwiraj Bhattacharjee </div>
+                 <div class="job">Lecturer <br>
+                 Computer Science & Engineering <br>
+                 Co-Advisor <br>
+                 Leading University Computer Club (LUCC)</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./Lucc/dipto1-scaled-1.jpg" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Dipta Chandra Paul
+                 </div>
+                 <div class="job">Lecturer <br>
+                 Computer Science & Engineering <br>
+                 Co-Advisor <br>
+                 Leading University Computer Club (LUCC)</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+       </div>
+      
+    </div>
+</section>
+
+<section class="gallery-section" >
+<div class="gallery-div">
+
+<div class="gallery-wrap">
+    <div class="gallery-images">
+        <div class="gallery">
+          <div class="image-section">
+            <img src="./Lucc/event/TECH STORM.jpg" alt="">
+          </div>
+          <div class="caption">
+            <h3>TECH STORM 5</h3>
+            <p>Leading University Computer Club (LUCC) will host the TECH STORM 5 Powered By Hi-Fi Computer at Leading University. The event is titled "LUCC PRESENTS TECH STORM 5" and will span two days, from December 8th to 9th, on our permanent campus. </p>
+          </div>
+        </div>
+        <div class="gallery">
+            <div class="image-section">
+              <img src="./Lucc/event/TECH STORM-1.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3> Tech Storm 5: T-shirt Design </h3>
+              <p>Register today to secure your spot and grab the exclusive Tech Storm 5 T-shirt as a token of this amazing event. Join us and become a part of the legacy while exploring the world of technology, design, and gaming!</p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Lucc/event/TECH STORM-2.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>𝐋𝐔𝐂𝐂 𝐏𝐫𝐞𝐬𝐞𝐧𝐭 𝐉𝐮𝐧𝐢𝐨𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠 𝐂𝐨𝐧𝐭𝐞𝐬𝐭 𝐑𝐞𝐬𝐮𝐥𝐭𝐬!</h3>
+              <p>1st Place: Syeda Sadiatul Jannat Tushi (62nd Batch) - Solved 8 problems!  2nd Place: Swadheen Islam Robi (62nd Batch) - Solved 7 problems!  3rd Place: Sayed Al Mamun (61st Batch) - Solved 5 problems!</p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Lucc/event/TECH STORM-3.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>আপনাদের সার্বিক সহযোগিতায় ১ দিনে অর্ধলক্ষের উপরে টাকা উত্তোলন করা সম্ভব হয়েছে। আমরা অতিদ্রুত বন্যায় আক্রান্ত মানুষদের কাছে পৌঁছাতে চাই। তাই দ্রুত প্রয়োজন আপনাদের সার্বিক সহযোগিতা।</h3>
+              <p>এই সময়ে অসহায় ও বিপদগ্রস্থ মানুষের পাশে দাঁড়াতে এগিয়ে আসুন আমাদের সাথে -
+              </p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Lucc/event/TECH STORM-4.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>Heartfelt congratulations to Rumel M S Pir Sir on your prestigious appointment as the new Dean (in-charge) of the Modern Science Faculty</h3>
+              <p>As the Associate Professor and Head of the CSE Department, your unwavering dedication, visionary leadership, and exceptional contributions have been a beacon of inspiration to students and colleagues alike.</p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./Lucc/event/TECH STORM-5.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>'LU_dAREdevils', IUT 11th National ICT Fest Programming Contest 2024</h3>
+              <p>দলের ভিতরে ২৬ তম , ইউনিভার্সিটি ভিওিক ১৭ তম এবং
+প্রাইভেট ইউনিভার্সিটি মধ্যে ৫ম হওয়ার কৃতিত্ব অর্জন করেছে। দলটির সদস্য ছিলেন :
+১. আবু সুফিয়ান রাফি - ৫৭তম ব্যাচ
+(ACM Coordinator - Steering Committee 2023-‘24, LUCC)</p>
+            </div>
+          </div>
+    </div>
+</div>
+
+</div>
+</section>
    
   
     <footer>
