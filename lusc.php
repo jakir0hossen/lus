dd9cc5318c330cf7d19ef176258d2f36c42ce4a9
jakir0hossen <jhbjakir@gmail.com>
2024-12-14 22:05:04 +0600

    luts

---
+++ lusc.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>LU Magazine</title>
+    
+    <script src="https://kit.fontawesome.com/eaa4609b2f.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/79d1bfb033.js" crossorigin="anonymous"></script>
+   
+    <link rel="stylesheet" href="./lussc-style.css" />
+</head>
+
+<body>
+<nav>
+        <div class="container">
+            <div class="logo">
+                <img src="../images/logo.png" alt="">
+            </div>
+            
+
+            <div class="navbar">
+                <ul>
+                    <li><a href="../Home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="#">LU</a>
+                     <ul>
+                        <li><a href="./blog.php">Blogs</a></li>
+                        <li><a href="./books.php">Book</a></li>
+                        <li><a href="./event.php">Event</a></li>
+                        <li><a href="#">Lu Club</a>
+                            <ul>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUSSC</a></li>
+                                <li><a href="#">LUEC</a></li>
+                                <li><a href="#">LUCC</a></li>
+                                <li><a href="#">LUDC</a></li>
+                                <li><a href="#">LUMUNA</a></li>
+                                <li><a href="#">LU BNCC</a></li>
+                                <li><a href="#">LUPS</a></li>
+                                <li><a href="#">LUPS</a></li>
+                            </ul>
+                        </li>
+                     </ul>
+                    </li>
+                    <li><a href="./news.php">News</a></li>
+                    <li><a href="#">Gallery</a>
+                        
+                        <ul>
+                            <li><a href="./photo.php">Photo & Video</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="./contact.php">Contact</a></li>
+                    <li><a href="./logout.php"><i class="fa-solid fa-right-to-bracket"></i>logout</a></li>
+                </ul>
+                
+            </div>
+
+        </div>
+</nav>
+
+<section class="container-section">
+<div class="container-card">
+        <h1>Leading University Sports Club
+
+</h1>
+       <div class="main-card">
+         <div class="cards">
+           <div class="card">
+             <div class="content">
+               <div class="img">
+                 <img src="./lusc/lusc-1.webp" alt="">
+               </div>
+               <div class="details">
+                 <div class="name">Md. Mahbubur Rahaman
+                 </div>
+                 <div class="job">Assistant Professor Head <br>
+                 Tourism and Hospitality Management<br>
+                 Advisor <br>
+                 Leading University Sports Club</div>
+               </div>
+               <div class="media-icons">
+                 <a href="#"><i class="fab fa-facebook-f"></i></a>
+                 <a href="#"><i class="fab fa-twitter"></i></a>
+                 <a href="#"><i class="fab fa-instagram"></i></a>
+                 <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+             </div>
+           </div>
+           <!-- Add other cards similarly -->
+         </div>
+        
+       </div>
+      
+    </div>
+</section>
+
+<section class="gallery-section" >
+<div class="gallery-div">
+
+<div class="gallery-wrap">
+    <div class="gallery-images">
+        <div class="gallery">
+          <div class="image-section">
+            <img src="./lusc/lusc-2.jpg" alt="">
+          </div>
+          <div class="caption">
+            <h3>Leading University Sports Club</h3>
+            <p>"A massive shoutout to *Shakerzz* for stepping up as our official team jersey sponsor! Your belief in our journey fuels our passion on and off the field. We wear your name with pride, and we couldn't be more thankful for your unwavering support.
+With heartfelt appreciation,
+*LUSC* </p>
+          </div>
+        </div>
+        <div class="gallery">
+            <div class="image-section">
+              <img src="./lusc/lusc-3.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3> Leading University Sports Club</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./lusc/lusc-4.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>Leading University Sports Club</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./lusc/lusc-5.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>Leading University Sports Club</h3>
+              <p>
+              </p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./lusc/lusc-7.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>Leading University Sports Club</h3>
+              <p></p>
+            </div>
+          </div>
+          <div class="gallery">
+            <div class="image-section">
+              <img src="./lusc/lusc-6.jpg" alt="">
+            </div>
+            <div class="caption">
+              <h3>Leading University Sports Club</h3>
+              <p></p>
+            </div>
+          </div>
+    </div>
+</div>
+
+</div>
+</section>
+   
+  
+    <footer>
+        <div class="footerContainer">
+            <div class="socialIcons">
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-google-plus"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                
+            </div>
+            <div class="footerNav">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Lu</a></li>
+                </ul>
+            </div>
+            
+        </div>
+
+        <div class="footerBottom">
+            <p>Copyright &copy;2024; Design by <span class="designer">LuMagazine</span></p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+
+
+
+
+
